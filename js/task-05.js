a51a4372c584dc805691a66b8207ebbf6e0462db
YuliyaBondG <manyunka@ukr.net>
2023-02-10 20:54:56 +0200

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const inputE1 = document.querySelector("input#name-input");
+const outputE1 = document.querySelector("span#name-output");
+
+inputE1.addEventListener("input",(event) => {
+    outputE1.textContent = event.currentTarget.value;
+    if (inputE1.value === "") outputE1.textContent = `Anonymous`;
+});
