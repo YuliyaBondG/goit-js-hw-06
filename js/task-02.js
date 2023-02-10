ab352d208f1afad0de770abebadc78b216609642
YuliyaBondG <manyunka@ukr.net>
2023-02-10 18:54:51 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+
+const TotalIngredientsListEl = [];
+
+ingredients.forEach((item) => {
+  const ingredientsItemEl = document.createElement("li");
+  ingredientsItemEl.classList.add("item");
+  ingredientsItemEl.textContent = item;
+  TotalIngredientsListEl.push(ingredientsItemEl);
+});
+
+ingredientsListEl.append(...TotalIngredientsListEl);
