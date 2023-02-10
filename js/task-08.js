7150d7ab9526883f06674a1d29310e64d373a75c
YuliyaBondG <manyunka@ukr.net>
2023-02-10 21:51:42 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const loginFormE1 = document.querySelector ("form.login-form");
+loginFormE1.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formE1ements = event.currentTarget.elements;
+
+
+    const mail = formE1ements.email.value;
+    const password = formE1ements.password.value;
+
+
+    if (mail === "" || password === "") {
+        return alert(`Заповніть всі поля`);
+    } 
+
+    const onFormResult = {};
+    const formData = new FormData(event.currentTarget);
+    console.log(formData);
+    formData.forEach((value, name) => {
+        onFormResult[name] = value;
+    });
+
+
+    console.log(onFormResult);
+    loginFormE1.reset();
+}
