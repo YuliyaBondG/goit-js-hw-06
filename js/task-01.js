6363b198b6a680da7c74e2374e376c69fcf02a92
YuliyaBondG <manyunka@ukr.net>
2023-02-10 18:35:48 +0200

    Task 1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const itemsE1 = document.querySelectorAll("li.item");
+console.log("Number of categories:", itemsE1.length);
+
+itemsE1.forEach((item) => {
+    const itemsTitle = item.querySelector(".item > h2");
+    console.log("Category:", itemsTitle.textContent);
+    const itemsLength = item.querySelectorAll(".item > ul > li");
+    console.log("Elements:", itemsLength);
+});
