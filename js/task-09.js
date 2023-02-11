406ed680cc04ba5cfcaede7bf67f064420d4dba9
YuliyaBondG <manyunka@ukr.net>
2023-02-11 12:24:44 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColorE1 = document.querySelector("button.change-color");
+
+
+const spanColorE1 = document.querySelector("span.color");
+
+const bodyE1 = document.querySelector("body");
+
+
+btnChangeColorE1.addEventListener("click", switchColor);
+
+function switchColor() {
+  const randomColor = getRandomHexColor();
+
+  bodyE1.style.backgroundColor = randomColor;
+
+  spanColorE1.textContent = randomColor;
+}
