c6897ca204c2db5ea90a96afe1e976f099699199
YuliyaBondG <manyunka@ukr.net>
2023-02-12 08:48:09 +0200

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -20,3 +20,18 @@ const markup = images
 	.join('')
 
   list.insertAdjacentHTML('beforeend', markup)
+
+
+//const galleryListEl = document.querySelector(".gallery");
+//const galleryItemEl = document.querySelector("li");
+//galleryListEl.style.display = "flex";
+//galleryListEl.style.justifyContent = "space-between";
+
+//const TotalGalleryListEl = [];
+
+//images.forEach((item) => {
+  //const itemEl = `<li><img class='gallery__img' src='${item.url}' alt='${item.alt}' width='300'/></li>`;
+  //TotalGalleryListEl.push(itemEl);
+//});
+
+//galleryListEl.insertAdjacentHTML("afterbegin", TotalGalleryListEl);
